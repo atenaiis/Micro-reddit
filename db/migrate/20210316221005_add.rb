97d4f8850ce8f222deb33487d563c5ddc97f98d0
atenaiis <attennaisc@gmail.com>
2021-03-16 16:20:04 -0600

    fixing foreign key code

--- db/migrate/20210316221005_add.rb
+++ db/migrate/20210316221005_add.rb
@@ -1,6 +1,5 @@
 class Add < ActiveRecord::Migration[6.1]
   def change
-    add_column :comments, :post_id, :integer
-    add_column :comments, :user_id, :integer
+    add_column :posts, :user_id, :integer
   end
 end
--- db/migrate/20210316221453_comments_to.rb
+++ db/migrate/20210316221453_comments_to.rb
@@ -1,4 +1,6 @@
 class CommentsTo < ActiveRecord::Migration[6.1]
   def change
+    add_column :comments, :post_id, :integer
+    add_column :comments, :user_id, :integer
   end
 end
