88c1ad29b7751d60395ba79929441c824a3c633a
atenaiis <attennaisc@gmail.com>
2021-03-16 17:56:57 -0600

    working with rubocop

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
-    validates :body, presence: true
-    has_many :comments
-    belongs_to :user
+  validates :body, presence: true
+  has_many :comments
+  belongs_to :user
   end
--- db/migrate/20210316221005_add.rb
+++ db/migrate/20210316221005_add.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Add < ActiveRecord::Migration[6.1]
   def change
     add_column :posts, :user_id, :integer
